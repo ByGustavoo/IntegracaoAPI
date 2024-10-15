5ec2117dbe7a1e053c28e2532333607cb7097a06
VR-GustavoCorrea <gustavo.correa@vrsoft.com.br>
2024-10-15 14:11:29 -0300

    INT-1
    - Commit de Inicialização

---
+++ src/main/java/br/com/integracaoapi/IntegracaoApplication.java
@@ -0,0 +1,12 @@
+package br.com.integracaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/integracaoapi/IntegracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.integracaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
