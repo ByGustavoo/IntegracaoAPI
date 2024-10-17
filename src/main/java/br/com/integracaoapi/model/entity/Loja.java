fee3cde001b29a6f45f7222ef56c237001698b5d
VR-GustavoCorrea <gustavo.correa@vrsoft.com.br>
2024-10-17 14:55:44 -0300

    INT-7
    - Desenvolvendo o endpoint de Loja
    - Entidade Loja
    - Classes criadas: Fornecedor e SituacaoCadastro

--- src/main/java/br/com/integracaoapi/model/entity/Loja.java
+++ src/main/java/br/com/integracaoapi/model/entity/Loja.java
@@ -1,12 +1,30 @@
 package br.com.integracaoapi.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "loja")
+@Table(name = "loja", schema = "public")
 public class Loja {
 
     @Id
+    @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotNull
+    @Size(max = 25)
+    @Column(name = "descricao", length = 25, nullable = false)
+    private String descricao;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_fornecedor")
+    private Fornecedor fornecedor;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_situacaocadastro")
+    private SituacaoCadastro situacaoCadastro;
 }
